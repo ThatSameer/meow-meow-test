14bd15b77f3a23cf627f181739b9b8d75a0d87d8
ThatSameer <sameerhussain5@hotmail.com>
2024-11-27 22:37:25 +0000

    feat: 🎸 add comms controller with id route param &  update test
    ✅ Closes: #4

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return the id from the route params', () => {
+      expect(appController.getNextDelivery('123')).toBe('The id is 123');
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('comms')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('your-next-delivery/:id')
+  getNextDelivery(@Param('id') id: string): string {
+    return `The id is ${id}`;
   }
 }
